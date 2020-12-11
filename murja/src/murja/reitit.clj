04073d5a77c71563235109aa8d418a0b74b965fd
Ilpo Lehtinen <feuer@feuerx.net>
2020-12-11 18:00:19 +0200

    Muuntaja seems to be in place, swagger-ui responds in localhost:3000/swagger/

--- murja/src/murja/reitit.clj
+++ murja/src/murja/reitit.clj
@@ -1,9 +1,13 @@
 (ns murja.reitit
   (:require [reitit.ring :as ring]
             [reitit.ring.middleware.muuntaja :as middleware.muuntaja]
+            [reitit.ring.middleware.parameters :as middleware.params]
+            [reitit.swagger-ui]
+            [reitit.swagger]
             [mount.core :as mount :refer [defstate]]
             [muuntaja.core :as muuntaja]
             [reitit.coercion.spec]
+            [reitit.ring.coercion :as ring.coercion]
             [org.httpkit.server :as http-kit]
             [clojure.tools.namespace.repl :as tn]))
 
@@ -16,15 +20,23 @@
   {:status 200
    :body "MOI"})
 
-(def app-routes [["/api" 
-                  ["/ping" {:get {:handler #'get-ping}}]]])
+(def app-routes [["" {:middleware [middleware.params/parameters-middleware]}
+                  ["/api" 
+                   ["/ping" {:get {:handler #'get-ping}}]]
+                  ["" {:no-doc true}
+                   ["/swagger.json" {:get (reitit.swagger/create-swagger-handler)}]
+                   ["/swagger/*" {:get (reitit.swagger-ui/create-swagger-ui-handler)}]]]])
 
 (defn router [options]
   (ring/router
    app-routes
    {:data {:coercion reitit.coercion.spec/coercion
            :muuntaja (create-muuntaja)
-           #_#_:middleware [middleware.muuntaja/format-middlewre]}}))
+           :middleware [middleware.muuntaja/format-middleware
+                        ring.coercion/coerce-exceptions-middleware
+                        ring.coercion/coerce-request-middleware
+                        ring.coercion/coerce-response-middleware]}}))
+                        
 
 (defn app [options]
   (ring/ring-handler (router options)
