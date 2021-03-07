86dbbe253e0257f0a8b2a18737f5391312fc5078
Ilpo Lehtinen <feuer@feuerx.net>
2021-03-07 07:15:52 +0200

    I added hugsql to the last place where clojure.java.jdbc was used, murja.db.users

--- murja/src/murja/db/users.clj
+++ murja/src/murja/db/users.clj
@@ -1,30 +1,34 @@
 (ns murja.db.users
   (:require [clojure.java.jdbc :as j]
+            [hugsql.core :refer [def-db-fns]]
             [murja.security :refer [sha-512]]))
 
-(defn update-user! [{:keys [db-spec]} {:keys [_id nickname img_location password]}]
-  (j/update! db-spec :blog.Users (cond-> {:Nickname nickname
-                                          :Img_location img_location
-                                          :password (sha-512 password)}
-                                   (empty? password) (dissoc :password)) ["ID = ?" _id]))
+(def-db-fns "user.sql")
 
+;; this is called from http-api /api/users/save
+(defn update-user! [{:keys [db-spec]} {:keys [_id nickname img_location password] :as user}]
+  (update-user* db-spec (if (empty? password)
+                          (dissoc user :password)
+                          (update user :password sha-512))))
+
+;; this is called from http-api /api/users/save
 (defn everything-is-empty? [{:keys [db-spec]}]
-  (= (j/query db-spec ["SELECT COUNT(*) AS Count FROM blog.Users"] {:row-fn :count
-                                                                    :result-set-fn first}) 0))
+  (zero? (:count (count-users db-spec))))
 
+;; this seems to be called from nowhere but repl 
 (defn register-user! [{:keys [db-spec]} nickname username img_location password]
   (j/with-db-transaction [d db-spec]
-    (let [everything-was-empty? (everything-is-empty? {:db-spec d})]
-
-      (j/insert! d :blog.Users
-                 [:Username :Nickname :Img_location :password]
-                 [username nickname img_location (sha-512 password)])
-      (let [user-id (j/query d ["SELECT u.id FROM blog.Users u WHERE u.Username = ?" username]
-                             {:row-fn :id
-                              :result-set-fn first})]
-        (j/insert! d :blog.GroupMapping
-                   [:UserID :GroupID :PrimaryGroup]
-                   [user-id (if everything-was-empty?
-                              1 ;; admin's id
-                              2 ;; user's id
-                              ) true])))))
+    (let [everything-was-empty? (everything-is-empty? {:db-spec d})
+          user {:nickname nickname
+                :username username
+                :img_location img_location
+                :password password}]
+                
+      (insert-user* d (update user :password sha-512))
+      (let [{user-id :id} (get-user d {:username username})]
+        (insert-groupmapping d {:userid user-id
+                                :groupid (if everything-was-empty?
+                                           1 ;; admin's id
+                                           2 ;; user's id
+                                           )
+                                :primarygroup true})))))
---
+++ murja/src/sql/user.sql
@@ -0,0 +1,20 @@
+-- :name count-users :? :1
+SELECT COUNT(*) AS Count FROM blog.Users
+
+-- :name update-user* :!
+update blog.Users
+set Nickname = :nickname,
+    Img_location = :img_location,
+    password = :password
+where ID = :_id
+
+-- :name insert-user* :!
+insert into blog.Users (username, nickname, img_location, password)
+values (:username, :nickname, :img_location, :password)
+
+-- :name get-user :? :!
+select * from blog.Users where username = :username
+
+-- :name insert-groupmapping
+insert into blog.groupmapping (userid, groupid, primarygroup)
+values (:userid, :groupid, :primarygroup)
