0a601098a092c23cffc06a2669b61e1ffc16048f
Ilpo Lehtinen <feuer@feuerx.net>
2022-12-05 23:22:33 +0200

    damned bash

--- environment/provision.sh
+++ environment/provision.sh
@@ -15,8 +15,7 @@ original_path=$(pwd)
 
 cd $original_path
 
-if [[ -f ../murja/target/murja-2.0.0-SNAPSHOT-standalone.jar]];
-then
+if [ -f ../murja/target/murja-2.0.0-SNAPSHOT-standalone.jar ]; then
     cp ../murja/target/murja-2.0.0-SNAPSHOT-standalone.jar ./murja.jar
     db_password=$(kpcli --kdb=./secrets.kdbx --command "show -f Root/db"|grep Pass:|cut -d: -f2|awk '{$1=$1};1')
     dns_api_key=$(kpcli --kdb=./secrets.kdbx --command "show -f Root/dns"|grep Pass:|cut -d: -f2|awk '{$1=$1};1')
@@ -25,7 +24,7 @@ then
 
     sed "s/{{dns}}/$dns_api_key/g" dns-conf/digitalocean.ini.template > dns-conf/digitalocean.ini
 
-    if [[ -d www]] ;
+    if [ -d www] ;
     then
 	rm -rf www
     fi
