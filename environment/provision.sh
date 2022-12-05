dd44aa5a473b0e37b00d94527b4001c2b1bdba7b
Ilpo Lehtinen <feuer@feuerx.net>
2022-12-05 22:48:21 +0200

    another iteration on the provisioning script

--- environment/provision.sh
+++ environment/provision.sh
@@ -19,8 +19,18 @@ if [[ -f ../murja/target/murja-2.0.0-SNAPSHOT-standalone.jar]];
 then
     cp ../murja/target/murja-2.0.0-SNAPSHOT-standalone.jar ./murja.jar
     db_password=$(kpcli --kdb=./secrets.kdbx --command "show -f Root/db"|grep Pass:|cut -d: -f2|awk '{$1=$1};1')
+    dns_api_key=$(kpcli --kdb=./secrets.kdbx --command "show -f Root/dns"|grep Pass:|cut -d: -f2|awk '{$1=$1};1')
     sed "s/{{db_password}}/$db_password/g" docker-compose.yml.template > docker-compose.yml
     sed "s/{{db_password}}/$db_password/g" config.edn.template > config.edn
+
+    sed "s/{{dns}}/$dns_api_key/g" dns-conf/digitalocean.ini.template > dns-conf/digitalocean.ini
+
+    if [[ -d www]] ;
+    then
+	rm -rf www
+    fi
+
+    git clone git@github.com:feuery/feuerx_frontpage.git www	
 
     docker-compose up
 else
